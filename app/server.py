01b7dc69ef0e47e7dd6a6327fd019af52e767c97
devkosal <devkosal@gmail.com>
2019-05-31 13:21:43 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/9t2o882799m7ygl/bridges.pkl?dl=1'
+export_file_name = 'bridges.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
